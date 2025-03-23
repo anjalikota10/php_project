16073a690da625b14322aa1d373f2b9f286c3ca1
root <root@ip-172-31-27-120.ec2.internal>
2025-03-23 11:47:55 +0000

    docker compose

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$conn=mysqli_connect("hostname","vertex", "vertex123" , "wedmegood");
+$conn=mysqli_connect("db","vertex", "vertex123" , "wedmegood");
  if($conn)
  {
     // echo"Connected";
